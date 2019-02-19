3a8a4ce8494010602cf164dab54ae16d3df80ec6
Bjornskjald <github@bjorn.ml>
2019-02-19 23:46:06 +0100

    Moved code into CommandLineRunner, added PathUtils

--- src/main/java/ml/bjorn/bow/botsofwar/BowApplication.java
+++ src/main/java/ml/bjorn/bow/botsofwar/BowApplication.java
@@ -1,34 +1,51 @@
 package ml.bjorn.bow.botsofwar;
 
 import ml.bjorn.bow.botsofwar.BotMap;
+import ml.bjorn.bow.botsofwar.utils.FileUtils;
+import ml.bjorn.bow.botsofwar.utils.PathUtils;
+
+import java.util.Arrays;
 
 import org.newdawn.slick.util.pathfinding.AStarPathFinder;
 import org.newdawn.slick.util.pathfinding.Path;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class BowApplication {
-    static final Logger log = LoggerFactory.getLogger(BowApplication.class);
+	static final Logger log = LoggerFactory.getLogger(BowApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(BowApplication.class, args);
-		RestTemplate restTemplate = new RestTemplate();
-        Integer[][] m = restTemplate.getForObject("http://bow.westeurope.cloudapp.azure.com:8080/getMap", Integer[][].class);
-        BotMap map = new BotMap(m);
-        log.info(map.toString());
-        AStarPathFinder pathFinder = new AStarPathFinder(map, 1000, false);
-        Path path = pathFinder.findPath(null,  0,  0,  6,  7);
-        for (int i = 0; i < path.getLength(); i++) {
-        	System.out.println(
-        		path.getStep(i).getX() +
-        		", " +
-        		path.getStep(i).getY()
-			);
-        }
-        log.info(map.toStringWithPath(path));
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			API api = new API(restTemplate);
+			
+			BotMap map = new BotMap(api.getMap());
+			BowApplication.mapHolder.setMap(map);
+			log.info(map.toString());
+			AStarPathFinder pathFinder = new AStarPathFinder(map, 1000, false);
+			Path path = pathFinder.findPath(null,  0,  0,  6,  7);
+			log.info(map.toString(path));
+			log.info(Arrays.toString(PathUtils.getMovesListFromPath(path)));
+			FileUtils.saveFile("./index.html", map.toHTMLTemplate());
+	
+			Entity[] entities = api.getEntites();
+			log.info(entities[0].toString());
+		};
 	}
 
 }
---
+++ src/main/java/ml/bjorn/bow/botsofwar/utils/PathUtils.java
@@ -0,0 +1,28 @@
+package ml.bjorn.bow.botsofwar.utils;
+
+import org.newdawn.slick.util.pathfinding.Path;
+
+public class PathUtils {
+	public static String[] getMovesListFromPath(Path path) {
+		String arr[] = new String[path.getLength() - 1];
+		for (int i = 0; i < path.getLength() - 1; i++) {
+			arr[i] = getStepDirection(path, i, false);
+		}
+		return arr;
+	}
+
+	private static String getStepDirection(Path path, int step, boolean useArrows) {
+		String arrows[] = {"↑", "↓", "←", "→"};
+		String letters[] = {"U", "D", "L", "R"};
+		String signs[] = useArrows ? arrows : letters;
+		int oldX = path.getStep(step).getX();
+		int oldY = path.getStep(step).getY();
+		int newX = path.getStep(step + 1).getX();
+		int newY = path.getStep(step + 1).getY();
+		if (oldY > newY) return signs[0];
+		if (oldY < newY) return signs[1];
+		if (oldX > newX) return signs[2];
+		if (oldX < newX) return signs[3];
+		return "";
+	}
+}
